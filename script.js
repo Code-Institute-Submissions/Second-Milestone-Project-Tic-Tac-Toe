a8da93c264c3a04427535726951aae60a0445b6e
ZitaBalint <zita.balint1072@gmail.com>
2021-01-10 02:13:22 +0000

    added js functions for x and o

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let x_class= 'x';
-let o_class= 'o';
-let columnElement = document.querySelectorAll('[data-column]');
+let x_class = 'x'
+let o_class = 'o'
+let columnElements = document.querySelectorAll('[data-column]');
+let oTurn;
 
 columnElements.forEach(column => {
     column.addEventListener('click', handleClick, {once:true})
@@ -8,8 +9,31 @@ columnElements.forEach(column => {
 
 function handleClick(e) {
     //mark
+let column = e.target;
+let currentClass;
+if (e == oTurn) {
+    currentClass = o_class;
+}
+else {
+    currentClass = x_class;
+}
+
+
+drawStep(column, currentClass)
+
+
+    //swap turns
     //check for win
     //check for drew
     //rematch
     //start new game 
+    changeTurns()
+}
+
+function drawStep(column, currentClass) {
+    column.classList.add(currentClass)
+}
+
+function changeTurns() {
+    oTurn= !oTurn;
 }
