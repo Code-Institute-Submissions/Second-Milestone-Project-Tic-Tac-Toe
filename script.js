c8a8ceceff8aa1dcaf170b7c642a4fed0fe7a081
ZitaBalint <zita.balint1072@gmail.com>
2021-02-02 07:29:41 +0000

    commented the js file

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let X_CLASS = 'x';
-let O_CLASS = 'o';
+
+//variables 
+
+let xClass = 'x';           //cell's x class in html
+let oClass = 'o';           //cell's x class in html
 let WIN_RESULT = [
     [0, 1, 2],
     [3, 4, 5],
@@ -9,41 +12,37 @@ let WIN_RESULT = [
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8]
-];
-let noWinner = [[0], [1], [2], [3], [4], [5], [6], [7], [8]];
-let gameResult = document.querySelector('.end-result');
-let gameResultText = document.querySelector('.game-result-text');
-let cellElements = document.querySelectorAll('[data-cell]');
+];                          // all the winning combinations for the game
+let noWinner = [[0], [1], [2], [3], [4], [5], [6], [7], [8]];       // the grid elements one by one 
+let gameResult = document.querySelector('.end-result');             // pop up modal in thwe ned of a match 
+let gameResultText = document.querySelector('.game-result-text');   // the modal announces the winner text
+let cellElements = document.querySelectorAll('[data-cell]');        // each of the cell elements in the grid
 let circleTurn;
-let playersForm = document.getElementById('playersForm');
-
-let playerX = document.getElementById('playerX');
-let playerO = document.getElementById('playerO');
-let playersSubmitO = document.getElementById('playersSubmitO');
-let playersSubmitX = document.getElementById('playersSubmitX');
-const soundX = document.getElementById('soundX');
-const soundO = document.getElementById('soundO');
-const winningSound = document.getElementById('winningSound');
-const tieSound = document.getElementById('tieSound');
-const timeIsUp = document.getElementById('timeIsUp');
-const gameSong = document.querySelector('.tic-tac-toe-song');
-const switchBox = document.getElementById('togBtn');
-
-let countdownClock = document.getElementById('countDown');
-let timeLeft = 60;
+let playerX = document.getElementById('playerX');                   //input of the playerX
+let playerO = document.getElementById('playerO');                   //input of the player O
+let playersSubmitO = document.getElementById('playersSubmitO');     //The value of the input of the player X
+let playersSubmitX = document.getElementById('playersSubmitX');     //The value of the input of the player O
+const soundX = document.getElementById('soundX');                   //sound of the X drawing
+const soundO = document.getElementById('soundO');                   //sound of the O drawing 
+const winningSound = document.getElementById('winningSound');       //sounfd of the check winner true 
+const tieSound = document.getElementById('tieSound');               //sound if the game is tie 
+const gameSong = document.querySelector('.tic-tac-toe-song');       //tic-tac-toe song in modal
+const switchBox = document.getElementById('togBtn');                //background color switch button
+let countdownClock = document.getElementById('countDown');          // cound down clock
+let timeLeft = 60;                                                  // countdown set value for a minute (60 sec)
 let timer;
-let startScoreX = 0;
-let startScoreO = 0;
+let startScoreX = 0;                                                // set value of player x starting score 0
+let startScoreO = 0;                                                // set value of player O starting score 0
 
-//Event Hanlers------------------------------------------------
+// the start of the page wehn loaded and the modal is on show 
 $(window).on('load', function () {
     $('#startingPage').modal('show');
 
-     $('.mute-button').show();
-    $('.sound-button').hide();
+     $('.mute-button').show();   //mute and sound button, sound is on mute is showing 
+    $('.sound-button').hide();   
 });
 
-
+// tic tac toe song pause and play 
 $('.game-song').on('click', function () {
     gameSong.play();
     $('#pauseBtn').show();
@@ -61,129 +60,114 @@ $('#playBtn').on('click', function () {
     $('#pauseBtn').show();
     $('#playBtn').hide();
 });
-
+//close song modal and it stops playing the song and resets to 0
 $('.close-song-modal').on('click', function () {
     gameSong.pause();
     $(gameSong).get(0).currentTime = 0;
 
-})
+});
 
 
-
+// submit form with players name and error messages in case name is too long or name is not entered to the inputs 
 $('#submitBtn').on('click', function (e) {
     let errorMessages = [];
-    if (playerX.value === '' || playerX.value == null) {
-        errorMessages.push("Player's name is required");
-    } else if (playerO.value === '' || playerO.value == null) {
+    if (playerX.value === '' || playerX.value === null) {
+        errorMessages.push("Player's name is required");                  // if not entered any value
+    } else if (playerO.value === '' || playerO.value === null) {
         errorMessages.push("Player's name is required");
     }
 
     if (playerX.value.length >= '7') {
         errorMessages.push("Player's name max 6 character");
-    } else if (playerO.value.length >= '7') {
+    } else if (playerO.value.length >= '7') {                           // if entered value is longer than 6 character 
         errorMessages.push("Player's name max 6 character");
     }
 
     if (errorMessages.length > 0) {
         e.preventDefault();
-        document.getElementById('error').innerText = errorMessages.join(' , ');
+        document.getElementById('error').innerText = errorMessages.join(' , ');   // display error message 
     }
 
     else {
         $('#playersSubmitO').val(playerO.value);
         $('#playersSubmitX').val(playerX.value);
         $('#playersTurn').text('next turn: ' + playersSubmitX.value);
-        $('.score-nameX').text(playersSubmitX.value);
+        $('.score-nameX').text(playersSubmitX.value);                               // else the modal is hide and the values get submitted
         $('.score-nameO').text(playersSubmitO.value);
-        $('#startingPage').modal('hide')
+        $('#startingPage').modal('hide');
         timer = setInterval(countdown, 1000);
     }
-       
-
 });
 
+// count down function 
 function countdown() {
     let minute = Math.floor(timeLeft / 60);
-    let seconds = Math.floor(timeLeft % 60);
+    let seconds = Math.floor(timeLeft % 60);                    
 
     countdownClock.innerHTML = minute + ":" + seconds;
     timeLeft--;
 
     if (timeLeft <= 0) {
         clearInterval(timeLeft = 0);
-        countdownClock.innerHTML = 'the time is up'
-        gameResultText.innerHTML = "Game over! No winner!"
-        gameResult.classList.add('show')
+        countdownClock.innerHTML = 'the time is up';
+        gameResultText.innerHTML = "Game over! No winner!";     // when time is up result message shown in a modal 
+        gameResult.classList.add('show');
         $(gameResult).show();
     }
     
-};
+}
 
 
-//x and o event handler 
+// handle click for the game, every clicked made for the grid, and in the cells 
 const handleClick = function (e) {
     const cell = e.target;
-    let activeClass = X_CLASS || O_CLASS;
+    let activeClass = xClass || oClass; //either has an x or on o class in the grid 
     if (circleTurn) {
-        activeClass = O_CLASS;
-        document.getElementById('playersTurn').innerText= 'next turn: ' + playersSubmitX.value ;
+        activeClass = oClass;
+        document.getElementById('playersTurn').innerText= 'next turn: ' + playersSubmitX.value ; // dislpayers the next turn on the top of the page 
         soundO.play();
     } else {
-        activeClass = X_CLASS;
+        activeClass = xClass;
         document.getElementById('playersTurn').innerText = 'next turn: ' + playersSubmitO.value ;
         soundX.play();
     }
 
-
-
-
-    //show click
+    //show click, actually fill the grid with the players x or o 
     drawClick(cell, activeClass);
 
-    if (checkWinner(activeClass)) {
+    if (checkWinner(activeClass)) {    //if the winner wasn't find yet it allowes to draw an x or an o to the grid
         gameOver(false);
     } else if (checkTie()) {
-        gameOver(true);
+        gameOver(true);             // if all grids have been filled and no winner announced then it is a tie and there is no winner 
     }
 
-
-
     //next turn
-
     nextTurn();
 
-
     //cehecking for end result 
-
     checkWinner(activeClass);
 
 
     //checking for a tie 
-
     checkTie(activeClass);
-
-
-
-
 };
 
 //Event Listeners------------------------------------------------
 
 
 
-//background color
-
+//background color switch from light to dark 
 switchBox.addEventListener('change', function () {
     document.body.classList.toggle('black');
 });
 
+function changeColor() {
+    document.body.classList.toggle('black');
+}
 
 
 
-
-//x and o 
-
-
+//x and o  event listener, every cell can only be clicked once, otherwise there wouldbe multiple classes to the same grid and the game would be broken 
 cellElements.forEach(function (cell) {
     cell.addEventListener('click', handleClick, {
         once: true
@@ -192,9 +176,10 @@ cellElements.forEach(function (cell) {
 });
 
 
+// when players want to rematch, and want to keep the ame names, function removes existing classes ans eventlistener, pauses and resets the timer, and adds the same evenlistener again with the click once as well as the new reseted time of 60sec 
 $('#rematchButton').on('click', function () {
-    $('div').removeClass(X_CLASS);
-    $('div').removeClass(O_CLASS);
+    $('div').removeClass(xClass);
+    $('div').removeClass(oClass);
     $(gameResult).hide();
     cellElements.forEach(function (cell) {
         cell.addEventListener('click', handleClick, { once: true });
@@ -207,20 +192,7 @@ $('#rematchButton').on('click', function () {
 });
 
 
-
-
-//Function----------------------------------------------------------
-
-
-//background color
-function changeColor() {
-    document.body.classList.toggle('black');
-}
-
-
-
-//x and o function 
-
+// game over function with a tie, a winner and an error. If winner is founbd message is displayed with the correct player's name, if a tie a tie message is displayed. In the case of error else statment apply
 function gameOver(checkTie) {
     
     if (checkTie) {
@@ -238,12 +210,12 @@ function gameOver(checkTie) {
         gameResultText.innerHTML = "Did you heck the system? &#128561";
 
     }
-    gameResult.classList.add('show');
+    gameResult.classList.add('show');           // adds the game result modal with the correct message and also clears countdown timer
     $(gameResult).show();
     clearInterval(timer);
-};
+}
 
-
+// score display 
 function setScore() {
     if (circleTurn) {
         startScoreO ++;
@@ -254,38 +226,45 @@ function setScore() {
     }
 }
 
+//function checks if cell has an active class and draws player x or o 
 function drawClick(cell, activeClass) {
     cell.classList.add(activeClass);
 }
 
+//turns between players
 function nextTurn() {
     circleTurn = !circleTurn;
 }
 
+//checking for the first true winning combination of the array, to meet the winner of the game 
 function checkWinner(activeClass) {
     return WIN_RESULT.some(value => {
-        return value.every(index => {
+        return value.every(index => {           
             return cellElements[index].classList.contains(activeClass);
         });
     });
 }
 
+// checks for tie, if every cell contains either an o or and x cell and winner was not announced 
 function checkTie(activeClass) {
     return noWinner.every(function (index) {
-        return cellElements[index].classList.contains(X_CLASS) || cellElements[index].classList.contains(O_CLASS);
+        return cellElements[index].classList.contains(xClass) || cellElements[index].classList.contains(oClass);
     });
 }
 
+//if players don't want to rematch but want to submit new players name then page reloads 
 $('#newgameButton').on('click', function () {
-    location.reload(true)
+    location.reload(true);
 });
 
+//mute button for all audio on the page 
 $('.mute-button').on('click', function () {
     $('audio').prop('muted', true);
     $('.sound-button').show();
     $('.mute-button').hide();
 });
 
+// oposite of mute butten, turns sounds back on 
 $('.sound-button').on('click', function () {
     $('audio').prop('muted', false);
     $('.mute-button').show();
